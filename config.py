f44a1fd3a2399e696a93e2ef74a0a113a4739e0b
1noadi <1noadisarker@gmail.com>
2024-11-04 19:12:09 +0600

    Update config.py

--- config.py
+++ config.py
@@ -6,17 +6,17 @@ from logging.handlers import RotatingFileHandler
 
 
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-API_ID = int(os.environ.get("API_ID", ""))
-API_HASH = os.environ.get("API_HASH", "")
+API_ID = int(os.environ.get("API_ID", "20077058"))
+API_HASH = os.environ.get("API_HASH", "349639a80eb5b1cb0ff6c2b8a6f16717")
 
 
-OWNER_ID = int(os.environ.get("OWNER_ID", ""))
-DB_URL = os.environ.get("DB_URL", "")
-DB_NAME = os.environ.get("DB_NAME", "madflixbotz")
+OWNER_ID = int(os.environ.get("OWNER_ID", "6380847042"))
+DB_URL = os.environ.get("DB_URL", "mongodb+srv://1noadisarkerrnuqHLoTLmV9A0oa@cluster0.4fimk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DB_NAME = os.environ.get("DB_NAME", "1noadisarker")
 
 
-CHANNEL_ID = int(os.environ.get("CHANNEL_ID", ""))
-FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "0"))
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "1002276681251"))
+FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "1002276681251"))
 
 
 FILE_AUTO_DELETE = int(os.getenv("FILE_AUTO_DELETE", "600")) # auto delete in seconds
@@ -28,7 +28,7 @@ TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
 
 try:
-    ADMINS=[6848088376]
+    ADMINS=[6380847042]
     for x in (os.environ.get("ADMINS", "6848088376").split()):
         ADMINS.append(int(x))
 except ValueError:
@@ -66,8 +66,8 @@ FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "Hello {mention}\n\n<b>You Need 
 
 
 
-ADMINS.append(OWNER_ID)
-ADMINS.append(6848088376)
+ADMINS.append(6380847042)
+ADMINS.append(6380847042)
 
 LOG_FILE_NAME = "filesharingbot.txt"
 
